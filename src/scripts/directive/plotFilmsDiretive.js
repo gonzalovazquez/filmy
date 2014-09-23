9d0ee1ce04b71d1fe72eb716f218ab2fb4d6989e
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-09-23 09:24:26 -0400

    linked movie data to nodes

--- src/scripts/directive/plotFilmsDiretive.js
+++ src/scripts/directive/plotFilmsDiretive.js
@@ -1,19 +1,29 @@
 filmy.directive('plotFilms', ['filmyService', 'd3Service', function(filmyService, d3Service) {
 	return {
 		restrict: 'EA',
+		scope: {
+			plotFilms: '=plotFilms'
+		},
 		link: function(scope, elem) {
 			d3Service.d3().then(function(d3) {
-				var circle = svg.selectAll("circle")
-				    .data([32, 57, 293], function(d) { return d; });
+				var data = scope.plotFilms,
+					body = d3.select(elem[0]),
+					svg, circle, circleAttr;
 
-				circle.enter().append("circle")
-				    .attr("cy", 60)
-				    .attr("cx", function(d, i) { return i * 100 + 30; })
-				    .attr("r", function(d) { return Math.sqrt(d); });
+				svg = body.append('svg')
+					.attr('width', 800)
+					.attr('height', 800);
 
-				circle.exit().remove();
+				circle = svg.selectAll('circle')
+						.data(data)
+						.enter()
+						.append('circle');
+
+				circleAttr = circle.attr('cx', function (d, i) { return (i + 1) * 20; })
+							.attr('cy', function (d, i) { return (i + 1) * 20; })
+							.attr('r', function (d) { return d.imdbRating; })
+							.style('fill', 'purple');
 			});
-		},
-		template: '<svg width="720" height="120"><circle cx="40" cy="60" r="10"></circle><circle cx="80" cy="60" r="10"></circle><circle cx="120" cy="60" r="10"></circle></svg>'
+		}
 	};
 }]);
--- src/scripts/service/d3Service.js
+++ src/scripts/service/d3Service.js
@@ -11,7 +11,8 @@ filmy.factory('d3Service', ['$document', '$q', '$rootScope',
 			var scriptTag = $document[0].createElement('script');
 			scriptTag.type = 'text/javascript'; 
 			scriptTag.async = true;
-			scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+			//scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+			scriptTag.src = 'bower_components/d3/d3.min.js';
 			scriptTag.onreadystatechange = function () {
 				if (this.readyState == 'complete') onScriptLoad();
 			}
