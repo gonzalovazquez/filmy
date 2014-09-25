c7f6b9b79488e8434a631e60b2248fcf8a39818f
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-09-25 18:22:06 -0400

    working prototype of node and label

--- src/scripts/directive/plotFilmsDiretive.js
+++ src/scripts/directive/plotFilmsDiretive.js
@@ -5,25 +5,46 @@ filmy.directive('plotFilms', ['filmyService', 'd3Service', function(filmyService
 			plotFilms: '=plotFilms'
 		},
 		link: function(scope, elem) {
-			d3Service.d3().then(function(d3) {
+			setTimeout(function() {
+				d3Service.d3().then(function(d3) {
 				var data = scope.plotFilms,
 					body = d3.select(elem[0]),
-					svg, circle, circleAttr;
+					labels, svg, node, 
+					nodeAttr, gnode;
 
 				svg = body.append('svg')
 					.attr('width', 800)
 					.attr('height', 800);
 
-				circle = svg.selectAll('circle')
+
+				gnode = svg.selectAll('g.gnode')
 						.data(data)
 						.enter()
-						.append('circle');
+						.append('g')
+						.classed('gnode', true)
+						.attr('transform', function(d, i) {
+							return 'translate(' + [ (i + 1) * 80] + ','+ [ (i + 1) * 80] +')'; 
+						});
 
-				circleAttr = circle.attr('cx', function (d, i) { return (i + 1) * 20; })
+
+				node = gnode.append('circle')
+						.attr('class', 'node');
+
+				nodeAttr = node.attr('cx', function (d, i) { return (i + 1) * 20; })
 							.attr('cy', function (d, i) { return (i + 1) * 20; })
 							.attr('r', function (d) { return d.imdbRating; })
 							.style('fill', 'purple');
-			});
+
+				labels = gnode.append('text')
+						.attr('x', function (d, i) { return (i + 1.5) * 20; })
+						.attr('y', function (d, i) { return (i + 1.5) * 20; })
+						.attr('class', 'node-label')
+						.attr('fill', 'white')
+						.text(function(d) { return d.title; });
+
+
+				});
+			}, 500);
 		}
 	};
 }]);
