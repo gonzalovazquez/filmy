32d381b8f2b9d3532134e94fc622aeb8ba140890
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-02-16 20:25:05 -0500

    updated config file

--- config/files.js
+++ config/files.js
@@ -1,6 +1,58 @@
-exports.lib = [
+/****************************
+*******Public Directory******
+****************************/
+
+exports.publicDir = 'public';
+
+exports.publicScripts = 'public/javascript';
+
+exports.publicStyles = 'public/css';
+
+exports.publicImage = 'public/images';
+
+/****************************
+*******Source Directory******
+****************************/
+exports.main = [
+	'src/scripts/app.js',
+	'src/scripts/**/*.js'
+];
+
+exports.index = 'src/index.html';
+
+exports.source = 'src';
+
+exports.scripts = 'src/scripts/**/*.js';
+
+exports.vendor = 'src/scripts/vendor/*js';
+
+exports.libs = [
 	'bower_components/**'
 ];
+
+exports.sass = [
+	'src/styles/*.sass'
+];
+
+exports.css = [
+	'src/styles/*.css'
+];
+
+exports.images = [
+	'src/images/*.png'
+];
+
+exports.views = [
+	'src/views/*.html'
+];
+
+exports.templates = [
+	'src/templates/*.html'
+];
+
+/****************************
+*******Test Directory******
+****************************/
 
 exports.libTest = [
 	'bower_components/angularjs/angular.js',
@@ -10,29 +62,6 @@ exports.libTest = [
 
 var testfiles = [
 	'test/unit/**/*.spec.js'
-];
-
-exports.main = [
-	'src/scripts/filmy.js',
-	'src/scripts/**/*.js'
-];
-
-exports.styles = [
-	'src/styles/*.scss',
-	'src/styles/*.css',
-	'src/styles/*.sass'
-];
-
-exports.views = [
-	'src/views/**'
-];
-
-exports.templates = [
-	'src/templates/*.html'
-];
-
-exports.fonts = [
-	'src/fonts/*'
 ];
 
 exports.test = exports.libTest.concat(exports.libTest, exports.main, testfiles);
